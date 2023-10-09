13818b98557687fdec2421a6483c548fa4e62bfb
HP <HP@DESKTOP-CPLQTK2>
2023-10-09 14:03:22 +0530

    SpringBoot JPA OneToMany Bidirectional Mapping

---
+++ src/main/java/com/springboot/SpringBootOneToManyBidirectionalMappingNormalMethodApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOneToManyBidirectionalMappingNormalMethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOneToManyBidirectionalMappingNormalMethodApplication.class, args);
+	}
+
+}
